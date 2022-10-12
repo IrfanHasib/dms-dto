7211ee7dea0e2965cf6969e37d9333075612cc5e
Irfan Hasib <irfan@internationalshowtimes.com>
2022-10-12 20:06:38 +0600

    add responseDtoValidator

--- dist/utils/dtoValidator.js
+++ dist/utils/dtoValidator.js
@@ -3,14 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.dtoValidator = void 0;
 const class_transformer_1 = require("class-transformer");
 const class_validator_1 = require("class-validator");
-const common_1 = require("@nestjs/common");
 const dtoValidator = async (dto, obj) => {
     const objInstance = (0, class_transformer_1.plainToClass)(dto, obj);
     const errors = await (0, class_validator_1.validate)(objInstance);
     if (errors.length > 0) {
-        throw new common_1.HttpException({
-            error: `validation failed. The error fields : ${errors.map(({ property }) => property)}`
-        }, 401);
+        throw new TypeError(`validation failed. The error fields : ${errors.map(({ property }) => property)}`);
     }
 };
 exports.dtoValidator = dtoValidator;
--- src/utils/dtoValidator.ts
+++ src/utils/dtoValidator.ts
@@ -1,6 +1,5 @@
 import { ClassConstructor, plainToClass } from "class-transformer";
 import { validate } from "class-validator";
-import { HttpException } from '@nestjs/common';
 
  const dtoValidator = async <T extends ClassConstructor<any>>(
     dto: T,
@@ -9,13 +8,11 @@ import { HttpException } from '@nestjs/common';
     const objInstance = plainToClass(dto, obj);
     const errors = await validate(objInstance);
     if (errors.length > 0) {
-        throw new HttpException(
-            {
-                error: `validation failed. The error fields : ${errors.map(
-                    ({ property }) => property
-                )}`
-            },
-            401,
+
+        throw new TypeError(
+            `validation failed. The error fields : ${errors.map(
+                ({ property }) => property
+            )}`
         );
     }
 };
