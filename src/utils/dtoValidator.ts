d869501209472b523cca1dda79194d3c629b4914
Irfan Hasib <irfan@internationalshowtimes.com>
2022-10-13 02:48:20 +0600

    remove duplicate entry

--- src/utils/dtoValidator.ts
+++ src/utils/dtoValidator.ts
@@ -6,7 +6,6 @@ const dtoValidator = async (dto: ClassConstructor<any>, obj: Object): Promise<st
   const errors = await validate(objInstance);
   let returnError: string[] = [];
   if (errors.length > 0) {
-    returnError.push('Response is not valid');
     errors?.map(({ constraints }) => {
       Object.values(constraints as Object)?.map(i => {
         returnError.push(i);
