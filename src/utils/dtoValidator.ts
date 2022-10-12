264bc7690fcc29608ceae039a505c77bd805bc13
Irfan Hasib <irfan@internationalshowtimes.com>
2022-10-12 19:56:58 +0600

    add http exception

--- src/utils/dtoValidator.ts
+++ src/utils/dtoValidator.ts
@@ -1,5 +1,6 @@
 import { ClassConstructor, plainToClass } from "class-transformer";
 import { validate } from "class-validator";
+import { HttpException } from '@nestjs/common';
 
  const dtoValidator = async <T extends ClassConstructor<any>>(
     dto: T,
@@ -8,10 +9,13 @@ import { validate } from "class-validator";
     const objInstance = plainToClass(dto, obj);
     const errors = await validate(objInstance);
     if (errors.length > 0) {
-        throw new TypeError(
-            `validation failed. The error fields : ${errors.map(
-                ({ property }) => property
-            )}`
+        throw new HttpException(
+            {
+                error: `validation failed. The error fields : ${errors.map(
+                    ({ property }) => property
+                )}`
+            },
+            401,
         );
     }
 };
