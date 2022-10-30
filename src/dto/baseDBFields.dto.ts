d77fcfb1aad6010f902ebd668c622fa05cc97b27
Irfan Hasib <irfan@internationalshowtimes.com>
2022-10-30 13:06:21 +0600

    move base fields to dto

--- src/dto/baseDBFields.dto.ts
+++ src/dto/baseDBFields.dto.ts
@@ -1,28 +1,39 @@
 import { IsBoolean, IsDate, IsInt, IsNotEmpty, IsNumber } from 'class-validator';
 import { Expose, Type } from 'class-transformer';
 import { decorate } from 'ts-mixer';
+import { Column, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
 export class BaseDBFieldsDto {
   @decorate(Expose())
   @decorate(IsNotEmpty())
   @decorate(IsNumber())
   @decorate(IsInt())
+  @decorate(PrimaryGeneratedColumn())
   id: number;
 
   @decorate(Expose())
   @decorate(IsNotEmpty())
   @decorate(IsBoolean())
+  @decorate(
+    Column({
+      nullable: false,
+      default: false,
+      type: 'boolean',
+    }),
+  )
   isDeleted: boolean;
 
   @decorate(Expose())
   @decorate(IsNotEmpty())
   @decorate(IsDate())
   @decorate(Type(() => Date))
+  @decorate(CreateDateColumn())
   createdAt: Date;
 
   @decorate(Expose())
   @decorate(IsNotEmpty())
   @decorate(IsDate())
   @decorate(Type(() => Date))
+  @decorate(UpdateDateColumn())
   updatedAt: Date;
 }
